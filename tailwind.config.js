395572e3f39807f45164b447b5bbe90727915284
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-10-15 21:58:59 +0545

    adding flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  content: [
+    './node_modules/flowbite/**/*.js'
+]
 }
